b2b408bb2c59e09e4f216dbec3cf85392047cf75
Ataches <jussanchezm@correo.udistrital.edu.co>
2022-06-23 21:37:36 -0500

    fix: lockorder diagrams

--- Examen/3-LockOrder/DirectoryManager.java
+++ Examen/3-LockOrder/DirectoryManager.java
@@ -1,22 +1,22 @@
 public class DirectoryManager {
 
     public static void main(String[] args) {
-        Directory dir_1 = new Directory("dir_1");
-        Directory dir_2 = new Directory("dir_2 ");
+        Directory dir_A = new Directory("dir_A");
+        Directory dir_B = new Directory("dir_B ");
         FileSysUtil_Rev fsRev = new FileSysUtil_Rev();
 
-        MoveProcess process = new MoveProcess(dir_1, dir_2, fsRev);
-        process.start();
+        MovementManager movementManager = new MovementManager(dir_A, dir_B, fsRev);
+        movementManager.start();
 
     }
 }
 
-class MoveProcess extends Thread {
+class MovementManager extends Thread {
     private Directory objDir_1;
     private Directory objDir_2;
     private FileSysUtil_Rev objFileSysUtil;
 
-    public MoveProcess(Directory dir_A, Directory dir_B, FileSysUtil_Rev fs) {
+    public MovementManager(Directory dir_A, Directory dir_B, FileSysUtil_Rev fs) {
         this.objDir_1 = dir_A;
         this.objDir_2 = dir_B;
         this.objFileSysUtil = fs;
